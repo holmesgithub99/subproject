19d47c39c9b8bf04103ae0aa3ba9e13ec9294b3b
Maksym Tatsenko <holmes@holmes-m-nb.clario.lcl>
2024-11-03 16:39:55 +0100

    add

--- my_project/lib/main.dart
+++ my_project/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        // This is the theme of your application. 3
         // This is the theme of your application. 2
         // This is the theme of your application. 1
         // This is the theme of your application.
